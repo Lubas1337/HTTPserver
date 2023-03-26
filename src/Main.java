cd7d8a73f541ce37dbcd2b6ff51a160e9b68af31
Lubas1337 <abylailuba@gmail.com>
2023-03-27 00:15:31 +0300

    Create server socket

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Main {
     public static void main(String[] args) {
-
+        int port = 8080;
+        try(ServerSocket serverSocket = new ServerSocket(port)){
+            System.out.println("Server started on " + port + " port");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
